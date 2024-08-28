18c6e76cb3adc92664f92168e9631764f155e668
handy <kyeungseok.kang@gmail.com>
2024-08-28 11:35:59 +0900

    feat :  shadcn 설치, 버튼컴포넌트  추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello, world!
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-1">
+      <Button>Click me</Button>
+      <Button size={"lg"}>Click me</Button>
     </main>
   );
 }
