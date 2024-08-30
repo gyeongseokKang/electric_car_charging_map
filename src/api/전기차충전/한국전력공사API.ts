6264dcba9b788b49071338b9b34351f07d6508e0
handy <kyeungseok.kang@gmail.com>
2024-08-30 22:52:36 +0900

    feat : 필터링 기능 구현

--- src/api/전기차충전/한국전력공사API.ts
+++ src/api/전기차충전/한국전력공사API.ts
@@ -1,3 +1,5 @@
+import { Station } from "./types";
+
 class 한국전력공사API {
   url = process.env.NEXT_PUBLIC_EV_CHARGING_API;
   serviceKey = process.env.NEXT_PUBLIC_EV_CHARGING_API_KEY;
@@ -9,7 +11,7 @@ class 한국전력공사API {
     page: number;
     perPage: number;
     addr: string;
-  }) {
+  }): Promise<Station[]> {
     if (!addr) {
       return [];
     }
@@ -18,7 +20,21 @@ class 한국전력공사API {
       `${this.url}/v1/getEvSearchList?page=${page}&perPage=${perPage}&cond%5Baddr%3A%3ALIKE%5D=${addr}&serviceKey=${this.serviceKey}`
     ).then((response) => response.json());
 
-    return response.data;
+    return response.data.map((station: any) => {
+      return {
+        csId: Number(station.csId),
+        csNm: station.csNm,
+        addr: station.addr,
+        lat: Number(station.lat),
+        longi: Number(station.longi),
+        cpId: Number(station.cpId),
+        cpNm: station.cpNm,
+        chargeTp: station.chargeTp,
+        cpTp: station.cpTp,
+        statUpdatetime: station.statUpdatetime,
+        cpStat: station.cpStat,
+      };
+    });
   }
 }
 
